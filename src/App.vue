e657dbb9831cbe304be1d66c28ca3e5c5f20412b
yuxs <yuxs2022@163.com>
2024-12-31 12:56:43 +0800

    Signed-off-by: yuxs <yuxs2022@163.com> -初始化资源库项目-241231

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HeaderIndex/>
+    <MainIndex/>
+    <FooterIndex/>
+  </div>
+</template>
+
+<script>
+import HeaderIndex from './components/header/HeaderIndex.vue'
+import FooterIndex from './components/footer/FooterIndex.vue'
+import MainIndex from './components/main/MainIndex.vue'
+export default {
+  name: 'app',
+  components: {
+    HeaderIndex,
+    FooterIndex,
+    MainIndex
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+</style>
